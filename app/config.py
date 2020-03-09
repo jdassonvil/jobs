c794fd7bd5884f0101855040b3708a2e7b835414
Jerome Dassonville <jerome.dassonville@datadoghq.com>
2020-03-09 11:21:11 +0100

    Fix filepath

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 def load_blacklist():
-    blacklist_filepath = os.path.join(os.path.dirname(os.path.dirname(__file__)), "BLACKLIST")
+    blacklist_filepath = os.path.join(os.path.dirname(__file__), "BLACKLIST")
     keyword_blacklist = []
     with open(blacklist_filepath) as f:
         for line in f:
