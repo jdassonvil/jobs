48ef2ce91233267045258ddb2fb3d21b52a3a082
Jerome Dassonville <jerome.dassonville@datadoghq.com>
2020-03-16 09:32:38 +0100

    fix

--- app/model/__init__.py
+++ app/model/__init__.py
@@ -3,4 +3,4 @@ from collections import namedtuple
 Job = namedtuple('Job', 'company title contract location href timestamp timetext')
 
 def from_mongo_doc(doc):
-    return Job(doc["company"], doc["title"], doc["contract"], doc["href"], doc["timestamp"], doc["timestamptext"])
+    return Job(doc["company"], doc["title"], doc["contract"], doc["href"], doc["timestamp"], doc["timetext"])
